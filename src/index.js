ac9253a72146716d7366b21af99f980475801a64
bubliksite <52692985+bubliksite@users.noreply.github.com>
2020-09-17 23:48:59 +0300

    Parallax Added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap"
 import "animate.css"
 
+import Parallax from 'parallax-js'
+var pink = document.getElementById('pink-girl');
+var parallaxInstance = new Parallax(pink);
+var green = document.getElementById('green-boy');
+var parallaxInstance = new Parallax(green);
+var pro = document.getElementById('oppo-pro');
+var parallaxInstance = new Parallax(pro);
+var lite = document.getElementById('oppo-lite');
+var parallaxInstance = new Parallax(lite);
+
 import "./components/script"
 import "./components/quiz"
 import "./components/forms"
