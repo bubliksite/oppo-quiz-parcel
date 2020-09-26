49046e9987a7c44955b1ef4adc99f93fb854fda1
bubliksite <52692985+bubliksite@users.noreply.github.com>
2020-09-26 11:55:58 +0300

    Full product page (sharing doesen't work)

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,3 +1,5 @@
+var resultShareImage
+
 $('document').ready(function($) {
 
     var result = [];
@@ -57,26 +59,48 @@ $('document').ready(function($) {
             if (result[i] == 'C') {C++}
             if (result[i] == 'D') {D++}
         }
+        var listPro = ['Режим Ultra Steady Video 3.0', '5G: новый стандарт скорости', ' Аккумулятор 65 Вт SuperVOOC 2.0', 'Частота обновления экрана 90 Гц']
+        var listLite = ['Камера с ИИ и 6 объективами', 'Экран Full HD+ с диагональю 6.43 дюйма', ' Тонкий и легкий корпус', '18 Вт быстрая зарядка']
+        function listingPro() {
+            for (let i = 0; i < listPro.length; i++) {
+                $('#result-phone-list').append('<li>' + listPro[i] + '</li>')
+            }
+            $('#result-phone-img').attr('src', 'res/result-pro.png')
+        }
+        function listingLite() {
+            for (let i = 0; i < listLite.length; i++) {
+                $('#result-phone-list').append('<li>' + listLite[i] + '</li>')
+                $('#result-phone-img').attr('src', 'res/result-lite.png')
+            }
+        }
         if (A == Math.max(A, B, C, D)) {
-            $('#answerSpan').text('#1 (A)')
-            $('#shareButtonsB').remove()
-            $('#shareButtonsC').remove()
-            $('#shareButtonsD').remove()
+            $('#answerSpan').text('контемпорари')
+            $('#result-subheader').text('Каждый день ты творишь — новую версию себя и окружающего мира, тонко чувствуешь ритм жизни и умеешь донести до окружающих свое мнение легко и убедительно.')
+            $('#result-description').text('Такой личности нужен мощный смартфон с серьезными возможностями. Reno4 Pro 5G оборудован мощным процессором Qualcomm® Snapdragon™ 765G, поддерживает сети 5G SA и 5G NSA и при этом отлично снимает даже ночью')
+            $('#result-image img').attr('src', 'res/res-cont.png')
+            listingPro()
+            //resultShareImage = '//korablik.studio/images/oppo/reno4/res-fb-con.jpg'
         } else if (B == Math.max(A, B, C, D)) {
-            $('#answerSpan').text('#2 (B)')
-            $('#shareButtonsA').remove()
-            $('#shareButtonsC').remove()
-            $('#shareButtonsD').remove()
+            $('#answerSpan').text('брейк-данс')
+            $('#result-subheader').text('Смотришь на мир со всех углов и не боишься экспериментов.')
+            $('#result-description').text('Такому мировоззрению нужен смартфон с соответствующими возможностями фото и видео. Благодаря тройной камере Reno4 Pro 5G  Ultra Clear с функцией лазерной автофокусировки вам удастся запечатлеть самые удивительные мгновения, свидетелями которых ты станешь, и вовремя сфокусироваться на самом важном. ')
+            $('#result-image img').attr('src', 'res/res-break.png')
+            listingPro()
+            //resultShareImage = '//korablik.studio/images/oppo/reno4/res-fb-br.jpg'
         } else if (C == Math.max(A, B, C, D)) {
-            $('#answerSpan').text('#3 (C)')
-            $('#shareButtonsA').remove()
-            $('#shareButtonsB').remove()
-            $('#shareButtonsD').remove()
+            $('#answerSpan').text('хип-хоп')
+            $('#result-subheader').text('Условности — это не для тебя. Твоя жизнь — огни больших городов и ритм широких улиц, ты делишься с миром своим собственным взглядом на эту жизнь, и он может резко отличаться от общепринятого.')
+            $('#result-description').text('Твой смартфон — давно не средство самовыражения, это инструмент — он должен быть надежным, быстрым и способным смотреть на все твоими глазами. Reno4 Lite — ультралегкий смартфон с потрясающими возможностями фото и видео — отличный выбор для решения твоих задач.')
+            $('#result-image img').attr('src', 'res/res-hip.png')
+            listingLite()
+            //resultShareImage = '//korablik.studio/images/oppo/reno4/res-fb-kiz.jpg'
         } else if (D == Math.max(A, B, C, D)) {
-            $('#answerSpan').text('#4 (D)')
-            $('#shareButtonsA').remove()
-            $('#shareButtonsB').remove()
-            $('#shareButtonsC').remove()
+            $('#answerSpan').text('кизомба')
+            $('#result-subheader').text('Общительная индивидуальность, умеешь идти по жизни соло, но предпочтешь интеллектуальное общение с партнером.')
+            $('#result-description').text('В твоем смартфоне есть и селфи, и веселые групповые фото, а видео – всегда захватывающие и динамичные. Такой же должен быть и телефон – легкий и самостоятельный, как ты. Reno4 Lite – ультралегкий, в элегантном черном цвете, устойчивый к появлению отпечатков – отличный компаньон. ')
+            $('#result-image img').attr('src', 'res/res-kiz.png')
+            listingLite()
+            //resultShareImage = '//korablik.studio/images/oppo/reno4/res-fb-hip.jpg'
         }
         $('#question' + countOfQuestions).css('display', 'none')
         $('#finalScreen').addClass('animate__animated animate__fadeInRight')
