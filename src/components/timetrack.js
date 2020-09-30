085de5a7add72b7363f7cc2bf700fb45f765c202
bubliksite <serj.solntsev@gmail.com>
2020-09-30 18:01:06 +0300

    Final production

---
+++ src/components/timetrack.js
@@ -0,0 +1,28 @@
+$('document').ready(function($) {
+    var currentURL = window.location.origin
+    if (currentURL == 'http://reno4.oppo.ru' || currentURL == 'https://reno4.oppo.ru') {
+        if (Date.now() > '1601542800000') {
+            $('.nav-link').removeClass('disabled')
+            $('#testYourself').removeClass('d-none')
+            $('#oppo-gift a').attr('href', '#product')
+            $('#product').removeClass('d-none')
+            $('footer').removeClass('d-none')
+        }
+        if (Date.now() > '1601618400000') {
+            $('#partners div').removeClass('d-none')
+        }
+        if (Date.now() > '1602187200000') {
+            //Действия на 23:00 08.10.20 (запуск продаж)
+        }
+        if (Date.now() > '1604178000000') {
+            //Действия на 00:00 01.11.20 (окончание акции)
+        }
+    } else {
+        $('.nav-link').removeClass('disabled')
+        $('#testYourself').removeClass('d-none')
+        $('#oppo-gift a').attr('href', '#product')
+        $('#product').removeClass('d-none')
+        $('footer').removeClass('d-none')
+        $('#partners div').removeClass('d-none')
+    }
+})
